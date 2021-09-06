41da1fdac1e02f0f41d81abe5c320ed774813c87
jevgenij-sukelovic <jevgenij.sukelovic@gmail.com>
2021-09-06 13:27:30 +0300

    first commit

---
+++ let number = 5.js
@@ -0,0 +1,22 @@
+"use strict";
+
+let numberOfFilms = +prompt('how mutch films are you watshed?','');
+
+const personalMoviesDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    geners: [],
+    privat: false
+};
+
+
+const a = prompt('what film are you watshig in last time',''),
+       b = prompt('what rate is for this film',''),
+       c = prompt('what film are you watshig in last time', ''),
+       d = prompt('what rate is for this film', '');
+
+personalMoviesDB.movies[a]=b;
+personalMoviesDB.movies[c]=d;
+
+console.log(personalMoviesDB);
