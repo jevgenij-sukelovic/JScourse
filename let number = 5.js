40404a6229ee932ceca50d6a95f1985294c0652c
jevgenij-sukelovic <jevgenij.sukelovic@gmail.com>
2021-09-12 22:47:46 +0300

    task number 3

--- let number = 5.js
+++ let number = 5.js
@@ -1,6 +1,17 @@
 "use strict";
 
-let numberOfFilms = +prompt('how mutch films are you watshed?','');
+let numberOfFilms;
+
+function start (){
+
+    numberOfFilms = +prompt('how mutch films are you watshed?','');
+
+    while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
+        numberOfFilms = +prompt('how mutch films are you watshed?','');
+    }
+}
+// start();
+
 
 const personalMoviesDB = {
     count: numberOfFilms,
@@ -9,30 +20,42 @@ const personalMoviesDB = {
     geners: [],
     privat: false
 };
-if(personalMoviesDB.count<10){
-    console.log('You watshed not many movies');
-}else if(personalMoviesDB.count>=10 || personalMoviesDB<=30){
-console.log('You are classic wiver');
-}else if(personalMoviesDB.count >30){
-    console.log('you are movie-man');
-}else{
-    console.log('error');
-}
 
-
- for(let i = 0; i<2; i++)
- {
-    let a = prompt('what is the last film you watshed','');
-    let b = prompt('what is the films reting','');
-
-    if(a != null && b!=null && a != '' && a.length < 50 &&  b != '' ){
-        personalMoviesDB.movies[a]=b;
+function myWatchedMovies(){
+    if(personalMoviesDB.count<10){
+        console.log('You watshed not many movies');
+    }else if(personalMoviesDB.count>=10 || personalMoviesDB<=30){
+    console.log('You are classic wiver');
+    }else if(personalMoviesDB.count >30){
+        console.log('you are movie-man');
     }else{
-        i--;
+        console.log('error');
     }
 
+}
+// myWatchedMovies();
 
- }
+
+function moviesRate(){
+
+    for(let i = 0; i<2; i++)
+    {
+       let a = prompt('what is the last film you watshed','');
+       let b = prompt('what is the films reting','');
+   
+       if(a != null && b!=null && a != '' && a.length < 50 &&  b != '' ){
+           personalMoviesDB.movies[a]=b;
+       }else{
+           i--;
+       }
+   
+   
+    }
+
+}
+// moviesRate();
+
+ 
 
 
 // const a = prompt('what is the last film you watshed','rembo'),
@@ -45,4 +68,31 @@ console.log('You are classic wiver');
 
 
 
-console.log(personalMoviesDB);
+
+
+function showMyDB(hidden){
+
+    if  (!hidden){
+        console.log(personalMoviesDB);
+    }
+
+}
+// showMyDB(personalMoviesDB.privat);
+
+function writeYourGeners(){
+
+    for(let i = 1; i<=3; i++){
+
+       
+
+        let gener = prompt (`Your liked gener by number ${i}`,'');
+
+        personalMoviesDB.geners[i-1]=gener;
+       
+    }
+
+}
+
+
+writeYourGeners();
+console.log(personalMoviesDB.geners);
